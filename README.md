2c955f9ce07987f8b5d7f7fc13d2d83ac53d12a6
Chiko <chiko@envs.net>
2025-05-17 21:00:55 +0100

    feat: first commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# Lizzy Flake
+
+This repository provides a nix flake for Lizzy, a customizable Waybar module for media playback that uses DBus signals.
+
+## Introduction
+
+Lizzy is a module designed for Waybar, a customizable status bar for Wayland compositors. It leverages DBus MPRIS signals to provide media playback information and controls directly on your Waybar.
+
+## Installation
+
+To install Lizzy using Nix, you can add this flake to your `flake.nix`:
+
+```nix
+{
+  inputs.lizzy-flake.url = "github:chikof/lizzy-flake";
+}
+```
+
+Then, include it in your system configuration or user environment.
+
+```nix
+environment.systemPackages = with pkgs; [
+    inputs.lizzy.packages.${pkgs.system}.lizzy
+];
+```
+
+Or
+
+```nix
+import = [
+    inputs.lizzy-flake.packages.${pkgs.system}.homeModules
+];
+```
+
+## License
+
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for more details.
---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "lizzy: a customizable Waybar media‑playback module";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        src = pkgs.fetchFromGitHub {
+          owner = "stefur";
+          repo = "lizzy";
+          rev = "main";
+          sha256 = "sha256-bq1CmjMbClflzEm2MwWHzlV3XqJh/YYqe2bkcj5mF1s=";
+        };
+        lizzyPkg = pkgs.rustPlatform.buildRustPackage {
+          pname = "lizzy";
+          version = "unstable";
+
+          src = src;
+          cargoLock = { lockFile = "${src}/Cargo.lock"; };
+
+          meta = with pkgs.lib; {
+            description =
+              "A customizable Waybar module for media playback via DBus MPRIS";
+            license = licenses.mit;
+            platforms = platforms.unix;
+          };
+        };
+      in {
+        packages = { lizzy = lizzyPkg; };
+
+        defaultPackage = lizzyPkg;
+
+        homeModules = { default = { home.packages = [ lizzyPkg ]; }; };
+
+        apps = {
+          lizzy = {
+            type = "app";
+            program = "${lizzyPkg}/bin/lizzy";
+          };
+        };
+      });
+}
